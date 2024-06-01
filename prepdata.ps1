7e70a6225b7dcc298cc92284f7f400dd0ea9e9fc
Vaibbavi Senthil Kumar <vaibbavis@microsoft.com>
2024-05-31 19:30:44 -0700

    update depends on

--- prepdata.ps1
+++ prepdata.ps1
@@ -20,13 +20,13 @@ Write-Host ""
 Write-Host 'Running "populate_sql.py"...'
 Write-Host ""
 $populatesqlArguments = "populate_sql.py",
-  "--sqlconnectionstring", "`"Driver={ODBC Driver 18 for SQL Server};Server=tcp:vskSqlServer-1.database.windows.net,1433;Database=vskSqlDatabase-1;Encrypt=yes;Connection Timeout=30;Authentication=ActiveDirectoryMsi`"",
+  "--sqlconnectionstring", "`"Driver={ODBC Driver 18 for SQL Server};Server=tcp:vskSqlServer-1.database.windows.net,1433;Database=vskSqlDatabase-1;UiD=a25d78c7-3abf-46c0-b36f-da798ffab694;Encrypt=yes;Connection Timeout=30;Authentication=ActiveDirectoryIntegrated`"",
   "--subscriptionid", "443b0799-279f-401e-a220-aa1463a7710f",
   "--resourcegroup", "vsk-demo-repo-rg",
   "--servername", "vskSqlServer-1",
   "-v"
 
-  Write-Warning "sql connection string $populatesqlArguments
+Write-Warning "sql connection string $populatesqlArguments"
 
 $process = Start-Process -FilePath $venvPythonPath -ArgumentList $populatesqlArguments -Wait -NoNewWindow -PassThru
 
