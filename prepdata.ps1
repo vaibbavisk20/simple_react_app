bda24a22007aa0c9dea15932bb5065628bbfb1d4
Vaibbavi Senthil Kumar <vaibbavis@microsoft.com>
2024-06-13 15:03:56 -0700

    update uid

--- prepdata.ps1
+++ prepdata.ps1
@@ -22,8 +22,9 @@ $process = Start-Process -FilePath $venvPythonPath -ArgumentList "-m pip install
 Write-Host ""
 Write-Host 'Running "populate_sql.py"...'
 Write-Host ""
+# prinicpal id: c424cd05-1d60-4d83-9ad2-933a63377713
 $populatesqlArguments = "populate_sql.py",
-  "--sqlconnectionstring", "`"Driver={ODBC Driver 18 for SQL Server};Server=tcp:vskSqlServer-613.database.windows.net,1433;Database=vskSqlDatabase-613;UiD=c424cd05-1d60-4d83-9ad2-933a63377713;Encrypt=yes;Connection Timeout=30;Authentication=ActiveDirectoryIntegrated`"",
+  "--sqlconnectionstring", "`"Driver={ODBC Driver 18 for SQL Server};Server=tcp:vskSqlServer-613.database.windows.net,1433;Database=vskSqlDatabase-613;UiD=f41e4c72-6df9-4dfe-9366-70dc573c62af;Encrypt=yes;Connection Timeout=30;Authentication=ActiveDirectoryIntegrated`"",
   "--subscriptionid", "443b0799-279f-401e-a220-aa1463a7710f",
   "--resourcegroup", "vskgh-rg",
   "--servername", "vskSqlServer-613",
